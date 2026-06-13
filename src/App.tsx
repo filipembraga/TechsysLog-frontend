f8767803ab4ac3ea64bfc8b07ae734e1a072c3ef
Filipe Braga <dev.filipe.braga@outlook.com>
2026-06-13 17:01:28 -0300

    Chore: add design system tokens and styles

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return (
+    <div className="bg-surface-base min-h-screen flex items-center justify-center">
+      <p className="text-brand font-mono text-2xl">TechsysLog</p>
+    </div>);
+}
